2abd21d5cdbdd45cf56b16d4eaef01e6b07b8484
Tyler Teufel <tdt0025@auburn.edu>
2024-02-19 20:57:22 -0600

    Current post setup

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,29 +1,28 @@
+import { clerkClient } from "@clerk/nextjs/server";
+
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+// const filterUserforClient = (user: User) => {
+//   return {
+//     id: user.id,
+//     username: user.username,
+//     profilePicture: user.profileImageUrl,
+//   };
+// };
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    return posts;
   }),
-
-  // create: publicProcedure
-  //   .input(z.object({ authorId: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     // simulate a slow db call
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     return ctx.db.post.create({
-  //       data: {
-  //         authorId: input.authorId,
-  //         content: "", // Add the 'content' property with an empty string value
-  //       },
-  //     });
-  //   }),
-
-  // getLatest: publicProcedure.query(({ ctx }) => {
-  //   return ctx.db.post.findFirst({
-  //     orderBy: { createdAt: "desc" },
-  //   });
-  // }),
 });
